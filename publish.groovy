119ff0bf8fd946e62185563fbe71c6c90c000945
galfrano <galfrano@gmail.com>
2020-06-05 14:30:17 +0200

    Update publish.groovy

--- publish.groovy
+++ publish.groovy
@@ -4,7 +4,7 @@ pipeline {
       stage('test') {
          steps {
             echo 'publishing'
-            sh "echo -e \"$NPM_USERNAME\n$NPM_PASSWORD\n$NPM_EMAIL\" | npm login"
+            sh 'echo -e "$NPM_USERNAME\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login'
             sh 'npm publish'
          }
       }
