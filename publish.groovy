edbbd97a5e2b6ad09f2188974e72c1d4fe53a804
gabriel <galfrano@gmail.com>
2020-06-05 13:36:58 +0200

    small changes

--- publish.groovy
+++ publish.groovy
@@ -1,21 +1,11 @@
-/*pipeline {
+pipeline {
    agent any
    stages {
       stage('test') {
          steps {
-           echo 'running unit tests'
-            sh 'npm install npx jest'
-            sh 'npx jest'
             echo 'publishing'
             sh 'npm publish'
          }
       }
    }
-}
-*/
-
-stage ('Publish') {
-  withNPM(npmrcConfig: 'galeria') {
-    sh 'npm publish'
-  }
 }
