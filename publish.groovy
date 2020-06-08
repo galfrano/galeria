8b5c055b9f40f9e4d6415eb623bd9d66c7f0f75d
gabriel <galfrano@gmail.com>
2020-06-08 11:34:56 +0200

    more jenkins tests

--- publish.groovy
+++ publish.groovy
@@ -4,8 +4,9 @@ pipeline {
       stage('test') {
          steps {
             echo 'publishing'
-            sh "echo -e \"$NPM_USERNAME\n$NPM_PASSWORD\n$NPM_EMAIL\" | npm login"
-            sh 'npm publish'
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: '5af8e9bd-34cf-4c11-99b3-5d6b62d1eeb6', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+              sh 'npm publish'
+            }
          }
       }
    }
