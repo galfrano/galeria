5515ba9a9a5da3aacd26085414b14911bd068792
gabriel <galfrano@gmail.com>
2020-06-08 12:17:20 +0200

    without token

--- publish.groovy
+++ publish.groovy
@@ -5,6 +5,24 @@ pipeline {
          steps {
             echo 'publishing'
             withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: '5af8e9bd-34cf-4c11-99b3-5d6b62d1eeb6', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+              sh 'npm login'
+              expect {
+                "Username:" {
+                  send $usernameVariable\r;
+                  expect {
+                    "Password:" {
+                      send $passwordVariable\r;
+                      expect{
+                        "Email: (this IS public)" {
+                          send "galfrano@gmail.com"\r;
+                          expect eof
+                          wait
+                        }
+                      }
+                    }
+                  }
+                }
+              }
               sh 'npm publish'
             }
          }
