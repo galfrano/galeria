cea688d95c23f9644cc561bb30071f7d84625dfd
gabriel <galfrano@gmail.com>
2020-06-08 13:43:42 +0200

    with token

--- publish.groovy
+++ publish.groovy
@@ -1,27 +1,11 @@
 pipeline {
    agent any
    stages {
-      stage('test') {
+      stage('publish') {
          steps {
             echo 'publishing'
-            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: '5af8e9bd-34cf-4c11-99b3-5d6b62d1eeb6', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-              sh 'npm login'
-              expect {
-                "Username:" {
-                  send "$usernameVariable\r";
-                  expect {
-                    "Password:" {
-                      send "$passwordVariable\r";
-                      expect{
-                        "Email: (this IS public)" {
-                          send "galfrano@gmail.com\r";
-                          expect eof
-                        }
-                      }
-                    }
-                  }
-                }
-              }
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'token', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+              sh 'npm config set //registry.npmjs.org/:_authToken ${PASSWORD}'
               sh 'npm publish'
             }
          }
