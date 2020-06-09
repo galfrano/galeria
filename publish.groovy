444b287041a6757500bc8d0af758600ad6453cfe
gabriel <galfrano@gmail.com>
2020-06-09 14:53:39 +0200

    changed context

--- publish.groovy
+++ publish.groovy
@@ -1,8 +1,9 @@
 
-def authenticate(){
+def publish(){
   withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: '5af8e9bd-34cf-4c11-99b3-5d6b62d1eeb6	', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]){
     def token = (username+":"+password).bytes.encodeBase64().toString()
     sh 'npm config set //registry.npmjs.org/:_authToken ${token}'
+    sh 'npm publish'
   }
 }
 
@@ -12,8 +13,7 @@ pipeline {
       stage('publish') {
          steps {
             echo 'publishing'
-            authenticate()
-            sh 'npm publish'
+            publish()
          }
       }
    }
