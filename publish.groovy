1e6bea712f9ec8d25f7812ac586978742eb23e96
gabriel <galfrano@gmail.com>
2020-06-09 14:50:10 +0200

    wrong repo

--- publish.groovy
+++ publish.groovy
@@ -2,7 +2,7 @@
 def authenticate(){
   withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: '5af8e9bd-34cf-4c11-99b3-5d6b62d1eeb6	', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]){
     def token = (username+":"+password).bytes.encodeBase64().toString()
-    sh 'npm config set //nexus.mgmt.conrad24.com/nexus/repository/npm-local/:_authToken ${token}'
+    sh 'npm config set //registry.npmjs.org/:_authToken ${token}'
   }
 }
 
