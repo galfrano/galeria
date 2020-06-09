2eaffb0abd849edacb279bfc66d00cd5741975f5
gabriel <galfrano@gmail.com>
2020-06-09 14:48:50 +0200

    now with breaking this with base64 as suggested, doubt it will work

--- publish.groovy
+++ publish.groovy
@@ -1,13 +1,19 @@
+
+def authenticate(){
+  withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: '5af8e9bd-34cf-4c11-99b3-5d6b62d1eeb6	', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]){
+    def token = (username+":"+password).bytes.encodeBase64().toString()
+    sh 'npm config set //nexus.mgmt.conrad24.com/nexus/repository/npm-local/:_authToken ${token}'
+  }
+}
+
 pipeline {
    agent any
    stages {
       stage('publish') {
          steps {
             echo 'publishing'
-            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'token', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-              sh 'npm config set //registry.npmjs.org/:_authToken ${PASSWORD}'
-              sh 'npm publish'
-            }
+            authenticate()
+            sh 'npm publish'
          }
       }
    }
