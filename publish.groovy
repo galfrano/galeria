6781ebf8d4821bcac46b06bb52d9b1d1e7f8004c
gabriel <galfrano@gmail.com>
2020-06-09 14:56:51 +0200

    debugging

--- publish.groovy
+++ publish.groovy
@@ -1,9 +1,9 @@
 
 def publish(){
-  withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: '5af8e9bd-34cf-4c11-99b3-5d6b62d1eeb6	', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]){
+  withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'token', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]){
     def token = (username+":"+password).bytes.encodeBase64().toString()
     echo token
-    sh 'npm config set //registry.npmjs.org/:_authToken ${token}'
+    sh 'npm config set //registry.npmjs.org/:_authToken ${PASSWORD}'
     sh 'npm publish'
   }
 }
